ff33da617d9160998e070fb6fd0272d561f9df1e
zuisho-1848-o <mizuki.hamada1848@gmail.com>
2023-12-16 23:36:02 +0900

    基本を整えたうえでデータも追加

--- docs/typing/typing.js
+++ docs/typing/typing.js
@@ -1,7 +1,29 @@
+class Utils {
+    static loadImage(path) {
+        return new Promise(resolve => {
+            const image = new Image();
+            image.addEventListener('load', () => {
+                resolve(image);
+            });
+            image.src = path;
+        });
+    }
+
+    static getShuffledArray(array) {
+        const arrayCopy = array.slice();
+        for (let i = array.length - 1; i > 0; i--) {
+            const rand = Math.floor(Math.random() * (i + 1));
+            [arrayCopy[i], arrayCopy[rand]] = [arrayCopy[rand], arrayCopy[i]];
+        }
+        return arrayCopy;
+    }
+}
+
+
+
 class Game {
-    constructor(canvasId, bgPath) {
-        this.bg = new Image();
-        this.bg.src = bgPath;
+    constructor(canvasId, bg) {
+        this.bg = bg;
         this.canvas = document.getElementById(canvasId);
         this.ctx = this.canvas.getContext('2d');
         this.currentWord = '';
@@ -15,11 +37,12 @@ class Game {
             { en: "minamotonoyoritomo", kana: "みなもとのよりとも", kanji: "源頼朝" },
             { en: "takedashingen", kana: "たけだしんげん", kanji: "武田信玄" },
             { en: "uesugikenshin", kana: "うえすぎけんしん", kanji: "上杉謙信" },
-            { en: "sakanouyeno tamuramaro", kana: "さかのうえのたむらまろ", kanji: "坂上田村麻呂" },
+            { en: "sakanouyenotamuramaro", kana: "さかのうえのたむらまろ", kanji: "坂上田村麻呂" },
             { en: "natsumesoseki", kana: "なつめそうせき", kanji: "夏目漱石" },
             { en: "kobodaishi", kana: "こうぼうだいし", kanji: "弘法大師" },
             { en: "hiragagennai", kana: "ひらがげんない", kanji: "平賀源内" }
         ];
+        this.currentWords = [];
         
         this.completedLetter = "";
 
@@ -28,9 +51,10 @@ class Game {
         this.ctx.font = '30px Arial';
     }
 
-    pickRandomWord() {
-        const index = Math.floor(Math.random() * this.words.length);
-        const word = this.words[index];
+    pickNextWord() {
+        if(this.currentWords.length === 0) this.currentWords = Utils.getShuffledArray(this.words);
+
+        const word = this.currentWords.pop();
         this.currentWord = word.en;
         this.currentKana = word.kana;
         this.currentKanji = word.kanji;
@@ -59,7 +83,8 @@ class Game {
     }
 
     start() {
-        this.pickRandomWord();
+        this.currentWords = Utils.getShuffledArray(this.words);
+        this.pickNextWord();
         this.updateGame();
     }
 
@@ -68,14 +93,19 @@ class Game {
             this.completedLetter += this.currentWord[this.wordIndex];
             this.wordIndex++;
             if (this.currentWord.length === this.wordIndex) {
-                this.pickRandomWord();
+                this.pickNextWord();
             }
         }
     }
 }
 
-const game = new Game('gameCanvas', "https://highsto.net/assets/images/story/overWorld_blue.webp");
-game.start();
 
-document.addEventListener('keydown', event => game.handleKeydown(event));
+const main = async () => {
+    const bg = await Utils.loadImage("assets/images/overWorld_blue.webp");
+    const game = new Game('gameCanvas', bg);
+    game.start();
 
+    document.addEventListener('keydown', event => game.handleKeydown(event));
+};
+
+main();
