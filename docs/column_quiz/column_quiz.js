5bf2361dae6a8f87bffff5549864ebcf99eeaedb
zuisho-1848-o <mizuki.hamada1848@gmail.com>
2024-03-01 15:34:51 +0900

    効果音・フォントを追加し、UIをさらに整えた

--- docs/column_quiz/column_quiz.js
+++ docs/column_quiz/column_quiz.js
@@ -3,16 +3,16 @@ const quizData = [
     { name: "織田信長", column: "攻撃を一点に集約せよ、無駄な事はするな。" },
     { name: "真田信繁", column: "真田日本一の兵。古よりの物語にもこれなき由。" },
     { name: "豊臣秀吉", column: "負けると思えば負ける、勝つと思えば勝つ。逆になろうと、人には勝つと言い聞かすべし。" },
-    { name: "足利義満", column: "たのむかな 我がみなもとの 岩清水 流れの末を 神をまかせて" },
+    { name: "足利義満", column: "たのむかな 我がみなもとの 岩清水<br>流れの末を 神をまかせて" },
     { name: "徳川家康", column: "人間はの、最も多くの人間を喜ばせたものが 最も大きく栄えるものじゃ。" },
     { name: "足利尊氏", column: "文武両道は車輪のごとし、一輪欠ければ人を渡さず。" },
     { name: "上杉謙信", column: "人の落ち目を見て攻め取るは、本意ならぬことなり。" },
     { name: "後醍醐天皇", column: "我が骨は吉野山の苔に埋まることになっても、我が魂は常に京の空を仰ぎ見続けるだろう。" },
-    { name: "武田信玄", column: "人は城 人は石垣 人は堀 情けは味方 仇は敵なり" },
+    { name: "武田信玄", column: "人は城 人は石垣 人は堀<br>情けは味方 仇は敵なり" },
     { name: "細川勝元", column: "短慮功を成さず。" },
     { name: "フランシスコ＝ザビエル", column: "熟した実は多くとも、それをもぎ取る人間が少なすぎる。" },
     { name: "北条氏康", column: "小事をおろそかにするべからず。" },
-    { name: "今川義元", column: "昨日なし 明日またしらぬ 人はただ 今日のうちこそ 命なりけれ" },
+    { name: "今川義元", column: "昨日なし 明日またしらぬ 人はただ<br>今日のうちこそ 命なりけれ" },
     { name: "山名宗全", column: "およそ例という文字をば、尚後は時という文字にかへて御心あるべし。" },
     { name: "足利義政", column: "花より外に色もなし。" },
     { name: "明智光秀", column: "敵は本能寺にあり…！！！" },
@@ -25,7 +25,7 @@ class Quiz {
         this.quizData = quizData;
         this.currentQuiz = 0;
         this.correctAnswers = 0;
-        this.choiceNum = 8;
+        this.choiceNum = 6;
         this.questionCount = 5; // デフォルトの問題数
         this.questions = [];
         this.waiting = false;
@@ -41,6 +41,9 @@ class Quiz {
         this.resultElement = document.getElementById('result');
         this.eachQResultElement = document.getElementById('eachQResult');
         this.questionCountSelect = document.getElementById('questionCountSelect');
+
+        this.correctCircleElement = document.getElementById('correctCircle');
+        this.wrongXElement = document.getElementById('wrongX'); 
 
         this.music = {
             thinking: new Audio('assets/audio/thinking.mp3'),
@@ -78,6 +81,8 @@ class Quiz {
     }
 
     nextQuestion() {
+        this.correctCircleElement.style.display = 'none';
+        this.wrongXElement.style.display = 'none';
         if (this.currentQuiz < this.questionCount) {
             this.music.question.currentTime = 0;
             this.music.question.play();
@@ -93,7 +98,7 @@ class Quiz {
         this.eachQResultElement.textContent = '';
         this.choicesElement.innerHTML = '';
         const question = this.questions[this.currentQuiz];
-        this.questionElement.textContent = question.column;
+        this.questionElement.innerHTML = question.column;
 
         let choices = [question.name];
         for (let i = 0; i < this.choiceNum - 1; i++) {
@@ -120,12 +125,14 @@ class Quiz {
         if (choice === correctAnswer) {
             this.music.correct.currentTime = 0;
             this.music.correct.play();
-            this.eachQResultElement.textContent = "正解！";
+            this.correctCircleElement.style.display = 'block';
+            this.eachQResultElement.textContent = `「${correctAnswer}」正解！`;
             this.correctAnswers++;
         } else {
             this.music.wrong.currentTime = 0;
             this.music.wrong.play();
-            this.eachQResultElement.textContent = `不正解。正解は「${correctAnswer}」でした。`;
+            this.wrongXElement.style.display = 'block';
+            this.eachQResultElement.textContent = `正解は「${correctAnswer}」でした。`;
         }
         this.currentQuiz++;
         this.waiting = true;
@@ -157,6 +164,7 @@ class Quiz {
             case 'quiz':
                 this.quizContainer.style.display = 'block';
                 this.music.thinking.currentTime = 0;
+                this.music.thinking.loop = true;
                 this.music.thinking.play();
                 break;
             case 'result':
